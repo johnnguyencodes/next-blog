0a1ab619330da09beb5b68babf2943e6888ae0ba
John Nguyen <jnguyen92780@gmail.com>
2024-08-11 13:37:27 -0700

    added hover effect, updated mdx styling

--- src/components/CardCategory.tsx
+++ src/components/CardCategory.tsx
@@ -17,7 +17,7 @@ export default function CardCategory({
   date: string;
 }) {
   return (
-    <Card className="h-[290px] w-[350px] shadow-lg">
+    <Card className="h-[290px] w-[350px] transform border-0 bg-lightmode-white transition-all duration-300 hover:translate-y-[-3px] hover:shadow-lg dark:bg-darkmode-black dark:shadow-darkmode-black-900">
       <CardHeader>
         <CardTitle>{title}</CardTitle>
       </CardHeader>
